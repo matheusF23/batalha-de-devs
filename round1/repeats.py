7545af6dfa0a255d48d67b36d0acdc6f3e556f5e
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-10-05 21:49:14 -0300

    computacao dos resultados do primeiro desafio

--- round1/repeats.py
+++ round1/repeats.py
@@ -1,11 +1,17 @@
 # Retornar a soma dos números que não se repetem
 from memory_profiler import profile
+import time
 
 l = [3,3,4,4,7,8]
+
+inicio = time.time()
 
 @profile
 def repeats(l):
     return sum([i for i in l if l.count(i) == 1])
+
+fim = time.time()
+print("Tempo de execução: ", fim-inicio)
 
 if __name__ == '__main__':
     repeats(l)
